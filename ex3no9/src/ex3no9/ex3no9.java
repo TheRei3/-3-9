a8e1c5b37d2bf3683869c7057ff82d7e31dd1754
admin <admin@DESKTOP-8L2IG92>
2021-04-16 06:00:36 +0900

    ex3no9

---
+++ ex3no9/src/ex3no9/ex3no9.java
@@ -0,0 +1,11 @@
+package ex3no9;
+
+public class ex3no9 {  //100p
+    public static void main(String[] args) {
+    	long a = 1_000_000 * 1_000_000;
+		long b = 1_000_000 * 1_000_000;
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+	}
+}
